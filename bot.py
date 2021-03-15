bb2e68c4018733d634ebd113f42bba2fbe37e824
lord-carlos <155855+lord-carlos@users.noreply.github.com>
2021-03-15 19:55:21 +0100

    first commit

---
+++ bot.py
@@ -0,0 +1,107 @@
+'''
+Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+    http://aws.amazon.com/apache2.0/
+
+or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+Modifications 2021 by RikiRC
+'''
+
+import sys
+import irc.bot
+import requests
+
+
+class TwitchBot(irc.bot.SingleServerIRCBot):
+    def __init__(self, username, client_id, token, channel):
+        self.client_id = client_id
+        self.token = token
+        self.channel = '#' + channel.lower()
+
+        # Get the channel id, we will need this for v5 API calls
+        url = 'https://api.twitch.tv/kraken/users?login=' + channel
+        headers = {'Client-ID': client_id,
+            'Accept': 'application/vnd.twitchtv.v5+json'}
+        r = requests.get(url, headers=headers).json()
+        self.channel_id = r['users'][0]['_id']
+
+        # Create IRC bot connection
+        server = 'irc.chat.twitch.tv'
+        port = 6667
+        print('Connecting to ' + server + ' on port ' + str(port) + '...')
+        irc.bot.SingleServerIRCBot.__init__(
+            self, [(server, port, 'oauth:'+self.token)], username, username)
+
+    def on_welcome(self, c, e):
+        print('Joining ' + self.channel)
+
+        # You must request specific capabilities before you can use them
+        c.cap('REQ', ':twitch.tv/membership')
+        c.cap('REQ', ':twitch.tv/tags')
+        c.cap('REQ', ':twitch.tv/commands')
+        c.join(self.channel)
+        print('Joined ' + self.channel)
+        c.privmsg(self.channel, "Connected!")
+
+    def on_pubmsg(self, c, e):
+
+        # If a chat messages is used in combination with a custom reward.
+        for row in e.tags:
+             if row['key'] == 'custom-reward-id':
+                rewardId = row['value']
+                self.do_reward_action(e, rewardId)
+
+
+        # If a chat message starts with an exclamation point, try to run it as a command
+        if e.arguments[0][:1] == '!':
+            cmd = e.arguments[0].split(' ')[0][1:]
+            print('Received command: ' + cmd)
+            self.do_command(e, cmd)
+        return
+
+    def do_reward_action(self, e, rewardId):
+        c = self.connection
+        if rewardId == '0c8fb25f-23e5-4642-a24f-50af5d0d03b1':
+            c.privmsg(self.channel, 'Oh daddy yeah: ' + e.arguments[0])
+
+
+    def do_command(self, e, cmd):
+        c = self.connection
+
+
+        # Poll the API the get the current status of the stream
+        elif cmd == "title":
+            url = 'https://api.twitch.tv/kraken/channels/' + self.channel_id
+            headers = {'Client-ID': self.client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
+            r = requests.get(url, headers=headers).json()
+            c.privmsg(self.channel, str(r['display_name']) + ' channel title is currently ' + str(r['status']))
+
+        # Provide basic information to viewers for specific commands
+        elif cmd == "raffle":
+            message = "This is an example bot, replace this text with your raffle text."
+            c.privmsg(self.channel, message)
+        elif cmd == "schedule":
+            message = "This is an example bot, replace this text with your schedule text."            
+            c.privmsg(self.channel, message)
+
+        # The command was not recognized
+        else:
+            c.privmsg(self.channel, "Did not understand command: " + cmd)
+
+def main():
+    if len(sys.argv) != 5:
+        print('Usage: twitchbot <username> <client id> <token> <channel>')
+        sys.exit(1)
+
+    username  = sys.argv[1]
+    client_id = sys.argv[2]
+    token     = sys.argv[3]
+    channel   = sys.argv[4]
+
+    bot = TwitchBot(username, client_id, token, channel)
+    bot.start()
+
+if __name__ == "__main__":
+    main()
